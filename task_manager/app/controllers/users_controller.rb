873af491251d0bc6c236baac781eda80f4f68c73
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-01 14:29:58 -0500

    modified user controller for parameters

--- task_manager/app/controllers/users_controller.rb
+++ task_manager/app/controllers/users_controller.rb
@@ -5,12 +5,17 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(params[:user])
+		@user = User.new(user_params)
 		if @user.save
 			redirect_to root_url, :notice => "Signed up!"
 		else
 			render "new"
 		end
 	end
-	
+
+	private
+  ## Strong Parameters 
+	def user_params
+    	params.require(:user).permit(:email, :password, :password_confirmation)
+  	end
 end
--- task_manager/app/models/user.rb
+++ task_manager/app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+
+	before_save :encrypt_password
+
+	validates_confirmation_of :password
+	validates_presence_of :password, :on => :create
+	validates_presence_of :email
+	validates_uniqueness_of :email
+
 	
 end
