822f9fb8c42d1f597cbec666892aa004822cb808
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-01 17:26:56 -0500

    Added Active column to User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201191034) do
+ActiveRecord::Schema.define(version: 20160201222509) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160201191034) do
     t.boolean  "manager"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.boolean  "active"
   end
 
 end
