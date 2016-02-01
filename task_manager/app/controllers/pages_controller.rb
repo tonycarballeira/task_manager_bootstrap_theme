b0471d41268d1d60f5e28f463acfabb0c1cb2538
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-01 16:16:10 -0500

    Added views for manager dash

--- task_manager/app/controllers/pages_controller.rb
+++ task_manager/app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
+
   def home
+  	@accounts = User.all
   end
+
+  def accounts
+  	unless current_user && current_user.manager
+  		redirect_to root_url, :notice => "You are not a Manager!"
+  	end
+  end
+
+  def manager_tasks
+  	unless current_user && current_user.manager
+  		redirect_to root_url, :notice => "You are not a Manager!"
+  	end
+  end
+
 end
