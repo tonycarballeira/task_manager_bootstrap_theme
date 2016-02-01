afcf052ffaca0846d81d918b9ac73c98fd7751d5
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-01 16:16:39 -0500

    Added views for manager dash, accounts and manager_accounts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
+
+  get "accounts" => "pages#accounts", :as => "accounts"
+  get "manage_tasks" => "pages#manager_tasks", :as => "manager_tasks"
   
   root :to => "pages#home"
 
