8bb1fc56dab29d09ab63ccaecf4a13e9d6e2e04f
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-01 15:03:48 -0500

    modified sessions controller for login

--- task_manager/app/controllers/sessions_controller.rb
+++ task_manager/app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.authenticate(session_params)
+  	
+  	if user
+    	session[:user_id] = user.id
+    	redirect_to root_url, :notice => "Logged in!"
+  	else
+    	flash.now.alert = "Invalid email or password"
+    	render "new"
+  	end
+  end
+
+  private
+  ## Strong Parameters 
+	def session_params
+    	params.permit(:email, :password)
+  	end
 end
--- task_manager/config/routes.rb
+++ task_manager/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
   
   root :to => "users#new"
-  
+
   resources :users
   resources :sessions
 
