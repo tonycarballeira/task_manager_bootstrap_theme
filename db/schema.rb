4e79b58754848c9e170779447587c36e27b19203
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-02 11:26:42 -0500

    Created Update Table and added User and Task foreign keys to it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202162059) do
+ActiveRecord::Schema.define(version: 20160202162415) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 20160202162059) do
   end
 
   add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
+
+  create_table "updates", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "task_id"
+  end
+
+  add_index "updates", ["task_id"], name: "index_updates_on_task_id"
+  add_index "updates", ["user_id"], name: "index_updates_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
