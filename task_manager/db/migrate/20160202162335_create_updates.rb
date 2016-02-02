4b81c72644d87856395d3ec9370763899370467c
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-02 11:25:57 -0500

    Created Update table and added user and test foreign key to it

---
+++ task_manager/db/migrate/20160202162335_create_updates.rb
@@ -0,0 +1,10 @@
+class CreateUpdates < ActiveRecord::Migration
+  def change
+    create_table :updates do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps null: false
+    end
+  end
+end
