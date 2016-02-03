8ea7faf6a252095a79fdc0b6560900aa31fcde92
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-03 14:19:51 -0500

    Initial setup of staffer Task show page

--- task_manager/app/controllers/pages_controller.rb
+++ task_manager/app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
   def home
-  	@user_tasks = Task.where(:user_id => current_user.id)
+  	if current_user && current_user.access == 3
+  		@user_tasks = Task.where(:user_id => current_user.id)
+  	end
   end
 
 end
