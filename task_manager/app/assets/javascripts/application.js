f0c02d34dcd9ee906c65b2283de98bec03ffaad1
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-03 18:40:55 -0500

    Utilized bootstrap for accounts list view

--- task_manager/app/assets/javascripts/application.js
+++ task_manager/app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $('.alert').fadeOut( "slow");
+  }, 2000);
+ })
+ 
