bbdd01ff7c67db81ab2f25365b553dc0765e2313
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-03 12:43:35 -0500

    Allow Staffer to view individual task details

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,45 +17,48 @@ class TasksController < ApplicationController
 
 	def edit
     	@task = Task.find(params[:id])
-  	end
+	end
 
-  	def update
-    	@task = Task.find(params[:id])
+	def update
+  	@task = Task.find(params[:id])
  
-    	if @task.update(task_params)
-      		redirect_to tasks_path, :notice => "Task Updated!"
-    	else
-      		render 'edit'
-    	end
+  	if @task.update(task_params)
+    		redirect_to tasks_path, :notice => "Task Updated!"
+  	else
+    		render 'edit'
   	end
+	end
 
-  	def index
-  		unless current_user && current_user.access < 3
-  			redirect_to root_url, :notice => "You are not a Manager!"
-  		end
+	def index
+		unless current_user && current_user.access < 3
+			redirect_to root_url, :notice => "You are not a Manager!"
+		end
 
-    	@tasks = Task.all
+  	@tasks = Task.all
 
-      if current_user.access == 2
+    if current_user.access == 2
 
-        @restricted_tasks = []
+      @restricted_tasks = []
 
-        Task.all.each do |x|
-          manager = User.where(:id => x.manager_id)[0]
+      Task.all.each do |x|
+        manager = User.where(:id => x.manager_id)[0]
 
-          if (manager.teams != []) && (manager.teams[0].id == current_user.teams[0].id)
-            @restricted_tasks << x
-          end
+        if (manager.teams != []) && (manager.teams[0].id == current_user.teams[0].id)
+          @restricted_tasks << x
         end
       end
+    end
+  end
 
-  	end
+  def show
+    @task = Task.find(params[:id])
+  end
 
 
 	private
   	
   	## Strong Parameters 
 	def task_params
-    	params.require(:task).permit(:title, :body, :user_id, :manager_id)
-  	end
+  	params.require(:task).permit(:id, :title, :body, :user_id, :manager_id)
+	end
 end
