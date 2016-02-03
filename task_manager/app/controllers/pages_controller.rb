a451d4dcacd0a0c5d16d3cb6b7ee8a734883a8ef
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-03 12:04:01 -0500

    Restricted Tasks visible to managers by team

--- task_manager/app/controllers/pages_controller.rb
+++ task_manager/app/controllers/pages_controller.rb
@@ -4,18 +4,4 @@ class PagesController < ApplicationController
   	
   end
 
-  def accounts
-  	unless current_user && current_user.access < 3
-  		redirect_to root_url, :notice => "You are not a Manager!"
-  	end
-  	@accounts = User.all
-  end
-
-  def manager_tasks
-  	unless current_user && current_user.access < 3
-  		redirect_to root_url, :notice => "You are not a Manager!"
-  	end
-    @tasks = Task.all
-  end
-
 end
