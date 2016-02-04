6e64e487d6f5101cc6850794ebdb27d117436ff6
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-04 14:39:48 -0500

    Added before filter to users and teams controllers to limit user access

--- task_manager/app/controllers/application_controller.rb
+++ task_manager/app/controllers/application_controller.rb
@@ -8,15 +8,15 @@ class ApplicationController < ActionController::Base
 
   	before_filter :time_zone
 
-  	def time_zone
-  		Time.zone = Time.now.zone
-  	end
-
 
 	private
 
 	def current_user
   		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
+
+  def time_zone
+    Time.zone = Time.now.zone
+  end
 
 end
