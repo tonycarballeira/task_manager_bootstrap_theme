3ce8b15b730651b9ebe4770ea28e682ec4896c13
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-12 16:32:02 -0500

    Modified home.html.erb and controllers to accomodate new dashboard

--- task_manager_2/app/controllers/pages_controller.rb
+++ task_manager_2/app/controllers/pages_controller.rb
@@ -2,9 +2,29 @@ class PagesController < ApplicationController
 
   def home
 
-  	if current_user && current_user.access == 3
-  		@tasks = Task.where(:user_id => current_user.id)
-  	end
+  	if current_user.access == 1
+
+      @tasks = Task.all
+      @users = User.all
+
+    elsif current_user.access == 2
+
+      @users = User.where(:access => 3)
+      @tasks = []
+
+      Task.all.each do |x|
+        manager = User.where(:id => x.manager_id)[0]
+
+        if (manager.teams != []) && (manager.teams[0].id == current_user.teams[0].id)
+          @tasks << x
+        end
+      end
+
+    else
+
+      @tasks = Task.where(:user_id => current_user.id)
+      
+    end
   	
   end
 
