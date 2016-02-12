15cccf4b7515199d149f6940e38bf9c6e111229d
Tony Carballeira <tonycarballeira@gmail.com>
2016-02-12 18:19:29 -0500

    Removed sidebar on login page

--- task_manager_2/config/initializers/assets.rb
+++ task_manager_2/config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( login.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
